325e9aaee05d556b81ea4c15087ef9adde2ab6a4
Prashanth <guntaprashanthkumar@gmail.com>
2024-08-15 12:11:57 +0530

    'update'

--- Travel_expenses/settings.py
+++ Travel_expenses/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'Travel_expenses.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [ BASE_DIR , 'template'],
+        'DIRS': [ BASE_DIR / 'template'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
